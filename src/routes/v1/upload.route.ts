3843c4a7c903ed2aff5546bbecd6c50a35d42eb8
Henrique Hermes <henriqueh.97@hotmail.com>
2024-04-18 10:03:03 +0800

    📦 NEW: upload route docs

--- src/routes/v1/upload.route.ts
+++ src/routes/v1/upload.route.ts
@@ -13,3 +13,37 @@ router
   .post(auth(), uploadController.multerConfig.single('file'), uploadController.upload);
 
 export default router;
+
+/**
+ * @swagger
+ * /upload:
+ *   post:
+ *     summary: Upload a file
+ *     description: Upload files to the backend.
+ *     tags: [Upload]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Upload'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
